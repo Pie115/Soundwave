0459c3bc99ea8726125ac358092f10895b96ff36
Rehan Ali <rali3@ucsc.edu>
2023-04-29 14:31:30 -0700

    got templates to work

--- Music_Recommender/frontend_code/views.py
+++ Music_Recommender/frontend_code/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+  
+# create a function
+def geeks_view(request):
+    # create a dictionary to pass
+    # data to the template
+    context ={
+        "data":"Gfg is the best",
+        "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+    # return response with template and context
+    return render(request, "template.html", context)
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+
+
+# def index(request):
+#     response = HttpResponse()
+#     response.write("<p>Test Text</p>")
+#     response.write("<p>Here's another paragraph</p>")
+#     return response
+# # Create your views here.
