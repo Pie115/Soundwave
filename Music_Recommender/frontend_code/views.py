5472b09b0bb1e7848c359ba1995c25c7877cc0f5
Rehan Ali <rali3@ucsc.edu>
2023-04-29 17:13:54 -0700

    created list maker

--- Music_Recommender/frontend_code/views.py
+++ Music_Recommender/frontend_code/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import NameForm
+
+
+values = []
   
 # create a function
 def geeks_view(request):
     # create a dictionary to pass
     # data to the template
+    # if len(values) > 0:
+    #     del(values[:])
+    if request.method == "POST":
+        form = NameForm(request.POST)   
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            value = list(cleaned_data.values())[0]
+            values.append(value)
+
     context ={
         "data":"Gfg is the best",
-        "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        "list": values,
+
+        # "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
     # return response with template and context
     return render(request, "template.html", context)
