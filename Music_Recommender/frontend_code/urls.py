5fcca7244066d7492db434fd94a3c515c437ae7b
Rehan Ali <rali3@ucsc.edu>
2023-04-29 18:21:01 -0700

    added song output page

--- Music_Recommender/frontend_code/urls.py
+++ Music_Recommender/frontend_code/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import geeks_view
+# from .views import geeks_view
+
+from .views import *
 
 urlpatterns = [
     path("", geeks_view, name="geeks_view"),
+    path("", new_view, name = "new_view"),
 ]
--- Music_Recommender/frontend_code/views.py
+++ Music_Recommender/frontend_code/views.py
@@ -10,7 +10,7 @@ def geeks_view(request):
     # create a dictionary to pass
     # data to the template
     # if len(values) > 0:
-    #     del(values[:])
+        # del(values[:])
     if request.method == "POST":
         form = NameForm(request.POST)   
         if form.is_valid():
@@ -27,6 +27,27 @@ def geeks_view(request):
     # return response with template and context
     return render(request, "template.html", context)
 
+
+def new_view(request):
+    return render(request, "song_output.html", {})
+# create a dictionary to pass
+    # # data to the template
+    # if len(values) > 0:
+    #     del(values[:])
+    # if request.method == "POST":
+    #     form = NameForm(request.POST)   
+    #     if form.is_valid():
+    #         cleaned_data = form.cleaned_data
+    #         value = list(cleaned_data.values())[0]
+    #         values.append(value)
+
+    # context ={
+    #     "data":"Gfg is the best",
+    #     "list": values,
+
+        # "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # }    
+    # return response with template and context
 
 
 
